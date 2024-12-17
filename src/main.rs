71e5f749d57db70ef6fb86daf5648e91f505235b
James Hardy <cyanjamesmail@gmail.com>
2024-12-17 06:55:07 +0000

    env logger

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,75 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+// #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-use eframe::egui;
+// use eframe::egui;
 
-fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "My egui App",
-        options,
-        Box::new(|cc| {
-            // This gives us image support:
-            egui_extras::install_image_loaders(&cc.egui_ctx);
+// fn main() -> eframe::Result {
+//     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+//     let options = eframe::NativeOptions {
+//         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+//         ..Default::default()
+//     };
+//     eframe::run_native(
+//         "My egui App",
+//         options,
+//         Box::new(|cc| {
+//             // This gives us image support:
+//             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Ok(Box::<MyApp>::default())
-        }),
-    )
+//             Ok(Box::<MyApp>::default())
+//         }),
+//     )
+// }
+
+// struct MyApp {
+//     name: String,
+//     age: u32,
+// }
+
+// impl Default for MyApp {
+//     fn default() -> Self {
+//         Self {
+//             name: "Arthur".to_owned(),
+//             age: 42,
+//         }
+//     }
+// }
+
+// impl eframe::App for MyApp {
+//     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+//         egui::CentralPanel::default().show(ctx, |ui| {
+//             ui.heading("My egui Application");
+//             ui.horizontal(|ui| {
+//                 let name_label = ui.label("Your name: ");
+//                 ui.text_edit_singleline(&mut self.name)
+//                     .labelled_by(name_label.id);
+//             });
+//             ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+//             if ui.button("Increment").clicked() {
+//                 self.age += 1;
+//             }
+//             ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+//             // ui.image(egui::include_image!(
+//             //     "../../../crates/egui/assets/ferris.png"
+//             // ));
+//         });
+//     }
+// }
+use env_logger::{Builder, Env, WriteStyle};
+use log::{debug, error, info, log_enabled, Level, Log};
+use std::{fmt::Debug, io::Write};
+
+mod csv;
+mod shared;
+
+fn init_logger() {
+    let env = Env::default().default_filter_or("info");
+    Builder::from_env(env).default_format().init();
 }
 
-struct MyApp {
-    name: String,
-    age: u32,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
-    }
-}
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-
-            // ui.image(egui::include_image!(
-            //     "../../../crates/egui/assets/ferris.png"
-            // ));
-        });
-    }
+fn main() {
+    // let shared = Share
+    init_logger();
+    log::info!("Hello World");
 }
---
+++ src/shared.rs
@@ -0,0 +1,7 @@
+use env_logger::Env;
+use log::{debug, error, log_enabled, info, Level};
+
+
+// struct Shared {
+//     logger: 
+// }
