d09e4ebc1ba46a0a7fa0fd730bc49bd4e45c448f
YammyToast <cyanjamesmail@gmail.com>
2025-01-06 14:56:09 +0000

    renderable docstrings

--- src/renderables.rs
+++ src/renderables.rs
@@ -14,6 +14,25 @@ use crate::templategen::{PageStyleConfig, TemplateProject};
 // ======================
 
 #[derive(Debug)]
+
+/// ## Text Renderable
+/// A 'text' object which can be rendered/written by a format writer, such as PDF.
+/// This object is saturated with data, and does not require any additional processing
+/// to be rendered.
+///
+/// Usage of this struct is encapsulated within the RenderableBuilder object. Instantiation
+/// of this struct directly outside of the builder is not intended, with exceptions for
+/// introspective testing.
+///
+/// ### Attributes
+/// - position: ProtocolBuffer-Position; the position of the object in cartesian X and Y.
+/// - lines: Strings of text, wherein each string is to be separated by a line break.
+/// - max_width: Maximum width of the 'text box'. Any text exceeding this width will be wrapped
+/// (into the lines vec).
+/// - max_height: Maximum height of the 'text box'. Should the cumulative wrapped text exceed
+/// this value, any additional text is hidden.
+/// - font_size: Font-size of the text to be rendered. Additionally used in text-wrapping calculations
+/// - font: TODO! Pointer to the font to be used for this text.
 pub struct TextRenderable {
     pub position: Position,
     pub lines: Vec<String>,
@@ -24,12 +43,17 @@ pub struct TextRenderable {
 }
 
 impl TextRenderable {
+    /// Divides provided text line into multiple substrings. Each substring should not
+    /// exceed the given max-width.
     fn wrap_text(__text: String, __max_width: f32, __glyph_width: f32) -> Vec<String> {
         // Calculate wrapping logic here!!!!
 
         return vec![__text];
     }
 
+    /// ## New From Text
+    /// Convert a Template-Text instance into a Renderable-Text instance.
+    /// This primarily populates the template with data from the working dataset.
     pub fn new_from_text(
         __text: Text,
         __value: String,
@@ -57,6 +81,21 @@ impl TextRenderable {
 // ======================
 
 #[derive(Debug)]
+/// ## Image Renderable
+/// An image object which can be rendered/written by a format writer, such as PDF.
+/// This object contains the image data, which is loaded from the path provided by the template
+/// it was spawned from.
+///
+/// Usage of this struct is encapsulated within the RenderableBuilder object. Instantiation
+/// of this struct directly outside of the builder is not intended, with exceptions for
+/// introspective testing.
+///
+/// ### Attributes
+/// - position: ProtocolBuffer-Position; the position of the object in cartesian X and Y.
+/// - image_path: Path to an image in the file-system.
+/// - image_data: DynamicImage object type from the Rust 'image' lib. Image data is held in an interoperable wrapper.
+/// - width: Width of the image.
+/// - height: Height of the image.
 pub struct ImageRenderable {
     pub position: Position,
     pub image_path: String,
@@ -66,6 +105,7 @@ pub struct ImageRenderable {
 }
 
 impl ImageRenderable {
+    /// Handles format specific loading logic for image data.
     fn handle_image_load(__img_path: &Path) -> Result<DynamicImage, Box<dyn std::error::Error>> {
         let ext = match __img_path.extension() {
             Some(ext) => ext,
@@ -94,6 +134,9 @@ impl ImageRenderable {
         return Ok(data);
     }
 
+    /// ## New From Image
+    /// Convert a Template-Image instance into a Renderable-Image instance.
+    /// This primarily loads the image data into a safe format from the image file-path.
     pub fn new_from_image(__image: Image) -> Result<Self, Box<dyn std::error::Error>> {
         let img_path = Path::new(&__image.image_path);
         // this should never throw unless this module is used in the
@@ -114,8 +157,6 @@ impl ImageRenderable {
             resize_height,
             image::imageops::FilterType::Lanczos3,
         );
-        
-
 
         let position = __image.position.unwrap();
         Ok(ImageRenderable {
@@ -133,6 +174,10 @@ impl ImageRenderable {
 // ======================
 
 #[derive(Debug)]
+/// ## RenderablePage
+/// Iterable Grouping Object, which holds together the properties and objects of each page to be rendered.
+/// As such, each page object contains the set of all renderables for the page, as well as the properties/style,
+/// such as height and width, of the page itself.
 pub struct RenderablePage {
     pub renderables: Vec<Box<dyn Any>>,
     pub page_style: PageStyle,
@@ -152,19 +197,37 @@ impl RenderablePage {
 // ======================
 
 #[derive(Debug)]
+/// ## RenderableBuilder
+/// Factory object which handles the population of a template with a loaded dataset.
+/// 
+/// Converts a template into a fully saturated, ordered collection of page groups, which can be 
+/// passed into any writer for output.
 pub struct RenderableBuilder {
     page_grouped_object_lists: Vec<RenderablePage>,
 }
 
 impl RenderableBuilder {
+    /// ## Convert Template Objects
+    /// Handles object specific conversion for Template-Objects into Renderable-Objects.
+    /// This method has to be called for each page in the output as conversions sometimes rely on data-record
+    /// specific logic.
+    /// 
+    /// ### Parameters
+    /// - __objects: Vector of unsorted TemplateObjects. DisplayObject is a simple pointer wrapper which enumerates
+    /// template objects into a sized.
+    /// - __record: Object which wraps around a single record/horizontal line of data in the dataset.
+    /// - __schema: Indexed columnar schema derived from the dataset being used. 
     fn convert_template_objects(
         __objects: Vec<DisplayObject>,
         __record: DataRecordIndexed,
         __schema: &DataProjectSchema,
     ) -> Vec<Box<dyn Any>> {
+        // Output vector which holds pointers to the list of dynamic renderable objects on heap.
         let mut out: Vec<Box<dyn Any>> = Vec::new();
+        // Iterate over template objects, match internal type to determine transformation logic.
         for object in __objects {
             match object.content {
+                // TEXT TEMPLATE OBJECTS
                 Some(Content::Text(v)) => {
                     // get the column, which narrows down to the element/value for this text object.
                     let text_value = &__record[&v.data_column];
@@ -177,6 +240,7 @@ impl RenderableBuilder {
                         Ok(v) => out.push(Box::new(v)),
                     }
                 }
+                // IMAGE TEMPLATE OBJECTS
                 Some(Content::Image(v)) => {
                     let renderable = ImageRenderable::new_from_image(v);
                     match renderable {
@@ -187,6 +251,7 @@ impl RenderableBuilder {
                         Ok(v) => out.push(Box::new(v)),
                     }
                 }
+                // Should never throw assuming objects are instantiated via a TemplateBuilder.
                 None => {
                     error!("Could not convert object: {:?} into renderable", object);
                     continue;
@@ -196,6 +261,10 @@ impl RenderableBuilder {
         return out;
     }
 
+    /// ## New From Template and Data
+    /// Create a new builder from a template and a dataset.
+    /// The TemplateProject and DataProject both represent fully parsed/handled items, which do not
+    /// require further mutability to their data to be viable for 'building' into the output.
     pub fn new_from_template_and_data(
         __templateproject: TemplateProject,
         __data: DataProject,
@@ -224,6 +293,7 @@ impl RenderableBuilder {
     }
 }
 
+/// Defines the behaviour for retreiving the products of the RenderableBuilder.
 impl IntoIterator for RenderableBuilder {
     type Item = RenderablePage;
 
