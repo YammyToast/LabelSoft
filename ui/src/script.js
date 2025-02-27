45d43f0dde8a1bc7197d3083bacc33cb29293ea7
YammyToast <cyanjamesmail@gmail.com>
2025-02-27 11:35:05 +0000

    aa

--- ui/src/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('ping').addEventListener('click', async () => {
-    const response = await window.api.sendToRust("ping");
-    document.getElementById('response').innerText = response.message;
-});
